f8a70f407a1af44b13a5e699ff61afd7df5b46a3
Scott Barrett <smarbar@gmail.com>
2021-09-03 21:49:57 +0100

    Added Comments

--- MSTeamsDirectRouting.psm1
+++ MSTeamsDirectRouting.psm1
@@ -1,3 +1,17 @@
+<#
+  MSTeamsDirectRouting
+  Module for Management of Teams Voice Configuration for Tenant and Users
+  User Configuration for Voice, Creation and connection of Resource Accounts,
+  Creation and Management of Call Queues and Auto Attendants
+  by Scott Barrett
+  https://github.com/smarbar
+.LINK
+  https://github.com/smarbar/MSTeamsDirectRouting/tree/main/docs
+#>
+
+# Exporting Module Members (Functions)
+Export-ModuleMember -Function $(Get-ChildItem -Include *.ps1 -Path $PSScriptRoot\Public\Functions -Recurse).BaseName
+
 Get-ChildItem -Filter *.ps1 -Path $PSScriptRoot\Public\Functions, $PSScriptRoot\Private\Functions -Recurse | ForEach-Object {
   . $_.FullName
 }
